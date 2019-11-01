54d771ff97f7320221e4d0808ff59feab63f32e6
Yifei Xing <yifeixing@Yifeis-MacBook-Pro.local>
2019-11-01 13:42:48 +0800

    some changes

--- search/views.py
+++ search/views.py
@@ -14,25 +14,35 @@ import string
 import random
 import pandas as pd
 from django.http import HttpResponseRedirect
-
+import pickle
 
 STOP_WORDS_FILENAME = './static/footer/stop_words.txt'
 data_location = "./static/footer/data.txt"
+authors_base = './static/footer/author.pkl'
+related_base = './static/footer/recommend.pkl'
+
 repository = searcher.BookInventory(data_location, STOP_WORDS_FILENAME)
 repository.load_books()
 docs_number = repository.books_count()
 all_fields = ['researchName', 'paperName', 'affiliation', 'field', 'journal']
 searched_query = {i: [] for i in all_fields}
 searched_query["results"] = {}
-default_researchers = ['John Denero', 'Jay Keasling', 'Stella Yu']
+default_researchers = ['John Denero-2936600175', 'Tze Leung Lai-2117483097', 'Alexei A. Efros-2088536091']
 df = pd.read_csv('./static/footer/nodup_joined.csv')
+
+with open(authors_base, 'rb') as f:
+    data_author = pickle.load(f)
+
+with open(related_base, 'rb') as g:
+    data_rec = pickle.load(g)
+
+
 sample = [["Jacob Kean", "Law and Economics~Regulation and Business Law", "#8DD3C7"], ["Jamie Reilly", "Mathematical and Quantitative Methods~Econometric and Statistical Methods:  Special Topic", "#FFFFB3"], ["Jonathan Peelle", "Mathematical and Quantitative Methods~Game Theory and Bargaining Theor", "#BEBADA"]]
 
 def details(request):
     #Request is only person, find all relevant details about this person
     timeTaken = time()
     authorID = request.GET['authorID']
-
     query = df.loc[df['author_id'] == np.float64(authorID)]
     if len(query) == 0:
         messages.error(request, 'Error: No researchers matched from database')
@@ -40,10 +50,35 @@ def details(request):
             researchers = default_researchers
         else:
             researchers = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+        researchers = [i.split("-") for i in researchers]
         context = {
             'researchers': researchers,
         }
         return HttpResponseRedirect('/search/')
+
+    try:
+        author_info = data_author[int(authorID)]
+        related_info = data_rec[int(authorID)]
+
+        collab = related_info['collaborator']
+        recommend = related_info['recommendation']
+
+        if author_info['ResearchInterst'] is not None:
+            interest = ' '.join(author_info['ResearchInterst'])
+        else:
+            interest = author_info['ResearchInterst']
+    except KeyError:
+        messages.error(request, 'Error: No researcher info matched from database')
+        if len(searched_query['researchName']) == 0:
+            researchers = default_researchers
+        else:
+            researchers = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+        researchers = [i.split("-") for i in researchers]
+        context = {
+            'researchers': researchers,
+        }
+        return HttpResponseRedirect('/search/')
+
 
     # temp_results = [{'coauthors': np.random.rand(), 'field': np.random.rand(), 'title': ''.join(random.choice(string.ascii_lowercase) for x in range(5))} for i in range(10)]
     image_url1 = query['URL'].values[0]
@@ -52,18 +87,27 @@ def details(request):
     pagerank = query['pageranks'].values[0]
     clustering = query['clustering'].values[0]
     eigencentrality = query['eigencentrality'].values[0]
+    coauthor_avg_citation = query['coauthor_avg_citation'].values[0]
+    coauthor_avg_publication = query['coauthor_avg_publication'].values[0]
     name = query['name'].values[0]
     end_time = time()
     timeTaken = int(end_time - timeTaken)
     context = {
         'imageurl1': image_url1,
         'imageurl2': image_url2,
-        'weighted_degree': weighted_degree,
-        'page': pagerank,
+        'weighted_degree': '{:g}'.format(float('{:.3g}'.format(weighted_degree))),
+        'page': '{:g}'.format(float('{:.3g}'.format(pagerank))),
         'timeTaken': timeTaken,
-        'cluster': clustering,
-        'eigen': eigencentrality,
+        'cluster': '{:g}'.format(float('{:.3g}'.format(clustering))),
+        'eigen': '{:g}'.format(float('{:.3g}'.format(eigencentrality))),
         'name': name,
+        'id': authorID,
+        'avg_cit': '{:g}'.format(float('{:.5g}'.format(coauthor_avg_citation))),
+        'avg_pub': '{:g}'.format(float('{:.5g}'.format(coauthor_avg_publication))),
+        'research': author_info,
+        'interest': interest,
+        'recommend': recommend,
+        'collab': collab,
         }
     return render(request, 'details.html', context)
 
@@ -74,6 +118,7 @@ def search(request):
             researchers = default_researchers
         else:
             researchers = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+        researchers = [i.split("-") for i in researchers]
         context = {
             'researchers': researchers,
         }
@@ -102,6 +147,7 @@ def search(request):
                 researchers = default_researchers
             else:
                 researchers = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+            researchers = [i.split("-") for i in researchers]
             context = {
                 'researchers': researchers,
             }
@@ -113,9 +159,9 @@ def search(request):
         for j in result:
             j['expertise'] = j['expertise'].replace(";", ' ')
             if j['professor'] in searched_query['results']:
-                searched_query['results'][j['professor']] += j['score']
+                searched_query['results'][j['professor']+'-'+j['id']] += j['score']
             else:
-                searched_query['results'][j['professor']] = j['score']
+                searched_query['results'][j['professor']+'-'+j['id']] = j['score']
         print(searched_query['results'])
         temp_results = result
         context = {
@@ -135,7 +181,15 @@ def search(request):
         return render(request, 'results.html', context)
     else:
         messages.error(request, 'Error: no queries')
-        return render(request, 'search.html')
+        if len(searched_query['researchName']) == 0:
+                researchers = default_researchers
+        else:
+            researchers = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+        researchers = [i.split("-") for i in researchers]
+        context = {
+            'researchers': researchers,
+        }
+        return render(request, 'search.html', context)
 
 
     
--- searchField/views.py
+++ searchField/views.py
@@ -14,20 +14,25 @@ import string
 import random
 import sys
 import pickle
+from django.http import HttpResponseRedirect
 
 
 STOP_WORDS_FILENAME = './static/footer/stop_words_topic.txt'
 data_location = "./static/footer/topic_data.txt"
 fields_base = "./static/footer/fields.pkl"
+paper_base = "./static/footer/papers.pkl"
 repository = topics.BookInventory(data_location, STOP_WORDS_FILENAME)
 repository.load_books()
 docs_number = repository.books_count()
 all_fields = ['fieldName', 'subfieldName']
 searched_query = {i: [] for i in all_fields}
 searched_query["results"] = {}
-default_fields = ['Fintech', 'Economics', 'Statistics']
+default_fields = ['Mathematical and Quantitative Methods-Data Collection and Data Estimation Methodology-C80', 'International Economics-Economic Impacts of Globalization-F60', 'Labor and Demographic Economics-Labor Standards: National and International-J80']
 with open(fields_base, 'rb') as f:
     data = pickle.load(f)
+
+with open(paper_base, 'rb') as g:
+    data_paper = pickle.load(g)
 
 
 
@@ -37,11 +42,19 @@ def details(request):
     field = request.GET['field']
     subfield = request.GET['subfield']
     # assert False, data
-
     ids = request.GET['id']
-    results = data[ids]
+    try:
+        results = data[ids]
+    except KeyError:
+        messages.error(request, 'Error: No related people matched from database')
+        return HttpResponseRedirect('/searchField/')
+    try:
+        relevant_papers = data_paper[ids]
+    except KeyError:
+        messages.error(request, 'Error: No related papers matched from database')
+        return HttpResponseRedirect('/searchField/')
 
-    
+
     # temp_results = [{'coauthors': np.random.rand(), 'field': np.random.rand(), 'title': ''.join(random.choice(string.ascii_lowercase) for x in range(5))} for i in range(10)]
     image_url1 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572436502562&di=5a296d58b1c1ca0cbd21c9fedde519fd&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Ffd1515ed28998c886a1c48acf7454f15a83b5b1b.jpg"
     image_url2 = "/static/footer/DaronAcemoglu.png"
@@ -55,6 +68,8 @@ def details(request):
         'field': field,
         'subfield': subfield,
         'ranking': results,
+        'papers': relevant_papers,
+        'iden': ids,
         }
     return render(request, 'detailsFields.html', context)
 
@@ -65,6 +80,7 @@ def search(request):
             fields = default_fields
         else:
             fields = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+        fields = [i.split("-") for i in fields]
         context = {
             'fields': fields,
         }
@@ -92,6 +108,8 @@ def search(request):
                 fields = default_researchers
             else:
                 fields = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+
+            fields = [i.split("-") for i in fields]
             context = {
                 'fields': fields,
             }
@@ -102,10 +120,10 @@ def search(request):
 
         for j in result:
             if j['field'] in searched_query['results']:
-                searched_query['results'][j['field']] += j['score']
+                searched_query['results'][j['field']+"-"+j['subfield']+'-'+j['code']] += j['score']
             else:
-                searched_query['results'][j['field']] = j['score']
-        print(result)
+                searched_query['results'][j['field']+"-"+j['subfield']+'-'+j['code']] = j['score']
+        print(searched_query)
         temp_results = result
         context = {
         'imageurl1': image_url1,
@@ -120,7 +138,16 @@ def search(request):
         return render(request, 'resultsFields.html', context)
     else:
         messages.error(request, 'Error: no queries')
-        return render(request, 'searchFields.html')
+        if len(searched_query['fieldName']) == 0:
+            fields = default_researchers
+        else:
+            fields = sorted(searched_query['results'].keys(), key=lambda x: searched_query['results'][x], reverse=True)[:5]
+
+        fields = [i.split("-") for i in fields]
+        context = {
+            'fields': fields,
+        }
+        return render(request, 'searchFields.html', context)
 
 
     
